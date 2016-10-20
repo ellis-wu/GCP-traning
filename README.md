6cc4d1f074c771e723efc6af7c99fd0542af6a38
ellisMing <j0989574647@gmail.com>
2016-10-20 11:21:57 +0800

    Add Cloud Resource Manager API

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 GCP APIs example.
 
 ## Before running
-
 1. If not already done, enable the Compute Engine API
    and check the quota for your project at
    [https://console.developers.google.com/apis/api/compute](https://console.developers.google.com/apis/api/compute)
@@ -21,21 +20,51 @@ GCP APIs example.
    ```
 
 ## Starting
+* [project operation](#project-operation)
+  * [list](#list)
+  * [create](#create)
+* [instance operation](#instance-operation)
+  * [list](#list-1)
+  * [get](#get)
+  * [create](#create-1)
 
-__*請以管理者權限進行操作！*__
+> * __*請以管理者權限進行操作！*__
+> * 若遇到 `Google Application Default Credentials` 問題，請參考：[Google Application Default Credentials](https://developers.google.com/identity/protocols/application-default-credentials)
 
-> 若遇到 `Google Application Default Credentials` 問題，請參考：[Google Application Default Credentials](https://developers.google.com/identity/protocols/application-default-credentials)
+## project operation
+Project manager 相關操作
+
+### list
+顯示所有 Project 的資訊：
+```sh
+$ ./gcpclient project list
+```
+
+### create
+建立一個新的 Project：
+```sh
+$ ./gcpclient project create {project name} {project ID}
+```
+
+> project_id 必須是唯一，不可重複。
+
+## instance operation
+
+GCP instance 相關操作
+
+### list
 
 顯示專案中所有 instance 的資訊：
 ```sh
 $ ./gcpclient instance list {project_id} {zone}
 ```
 
+### get
 取得 instance 的資訊：
 ```sh
 $ ./gcpclient instance get {project_id} {zone} {instance}
 ```
-
+### create
 建立一個基本的 `debian-8` instance：
 ```sh
 $ ./gcpclient instance create {project_id} {zone} {instance_id}
--- gcpclient.py
+++ gcpclient.py
@@ -4,20 +4,22 @@ import sys
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
+# project id = test-project-146302
+# region = asia-east1
+# zone = asia-east1-a
 
-def gcp_credential():
+def gcp_credential(api):
     try:
         credentials = GoogleCredentials.get_application_default()
-        service = discovery.build('compute', 'v1', credentials=credentials)
+        service = discovery.build(api, 'v1', credentials=credentials)
         return service
     except:
         sys.exit("Promise Undefined.")
 
 
 def gcp_instance_list(project, zone):
-    # region = 'asia-east1'
     try:
-        request = gcp_credential().instances().list(project=project, zone=zone)
+        request = gcp_credential('compute').instances().list(project=project, zone=zone)
         response = request.execute()
         encodedjson =  json.dumps(response, sort_keys=True, indent=4)
         print encodedjson
@@ -27,30 +29,28 @@ def gcp_instance_list(project, zone):
 
 def gcp_instance_get(project, zone, instance):
     try:
-        request = gcp_credential().instances().get(project=project, zone=zone, instance=instance)
+        request = gcp_credential('compute').instances().get(project=project, zone=zone, instance=instance)
         response = request.execute()
         encodedjson =  json.dumps(response, sort_keys=True, indent=4)
         print encodedjson
     except:
         sys.exit("instance get error")
 
+
 def gcp_instance_create(project, zone, instance_id):
     try:
         # Get the latest Debian Jessie image.
-        image_response = gcp_credential().images().getFromFamily(
+        image_response = gcp_credential('compute').images().getFromFamily(
             project='debian-cloud', family='debian-8').execute()
         source_disk_image = image_response['selfLink']
-
         # Configure the machine
         machine_type = "zones/%s/machineTypes/n1-standard-1" % zone
-
         # Configure th instance name
         name = instance_id
 
         config = {
             'name': name,
             'machineType': machine_type,
-
             # Specify the boot disk and the image to use as a source.
             'disks': [
                 {
@@ -61,7 +61,6 @@ def gcp_instance_create(project, zone, instance_id):
                     }
                 }
             ],
-
             # Specify a network interface with NAT to access the public internet.
             'networkInterfaces': [{
                 'network': 'global/networks/default',
@@ -69,7 +68,6 @@ def gcp_instance_create(project, zone, instance_id):
                     {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
                 ]
             }],
-
             # Allow the instance to access cloud storage and logging.
             'serviceAccounts': [{
                 'email': 'default',
@@ -78,7 +76,6 @@ def gcp_instance_create(project, zone, instance_id):
                     'https://www.googleapis.com/auth/logging.write'
                 ]
             }],
-
             # Metadata is readable from the instance and allows you to
             # pass configuration from deployment scripts to instances.
             "metadata": {
@@ -86,16 +83,24 @@ def gcp_instance_create(project, zone, instance_id):
                 "fingerprint": "-wmDBHKA7Qk="
             },
         }
-
-        gcp_credential().instances().insert(project=project, zone=zone, body=config).execute()
+        gcp_credential('compute').instances().insert(project=project, zone=zone, body=config).execute()
         print("create [%s] success" % name)
-
     except:
         sys.exit("create instance fail.")
 
+
+def gcp_instance_delete(project, zone, instance_id):
+    try:
+        request = gcp_credential('compute').instances().delete(project=project, zone=zone, instance=instance_id)
+        response = request.execute()
+        print("delete [%s] success" % instance_id)
+    except:
+        sys.exit("delete instance fail.")
+
+
 def gcp_instance():
     if len(sys.argv) < 3:
-        sys.exit('Usage: gcpclient instance {list|get|create}')
+        sys.exit('Usage: gcpclient instance {list|get|create|delete}')
     else:
         if sys.argv[2] == 'list':
             if len(sys.argv) != 5:
@@ -104,7 +109,6 @@ def gcp_instance():
                     "list project instance infomation"
                 )
             else:
-                # 'test-project-146302', 'asia-east1-a'
                 project = sys.argv[3]
                 zone = sys.argv[4]
                 gcp_instance_list(project, zone)
@@ -123,15 +127,73 @@ def gcp_instance():
             if len(sys.argv) != 6:
                 message_alert(
                     "Usage: gcpclient instance create {project_id} {zone} {instance_id}",
-                    "get instance infomation"
+                    "create instance"
                 )
             else:
                 project = sys.argv[3]
                 zone = sys.argv[4]
                 instance_id = sys.argv[5]
                 gcp_instance_create(project, zone, instance_id)
+        elif sys.argv[2] == 'delete':
+            if len(sys.argv) != 6:
+                message_alert(
+                    "Usage: gcpclient instance delete {project_id} {zone} {instance_id}",
+                    "delete instance"
+                )
+            else:
+                project = sys.argv[3]
+                zone = sys.argv[4]
+                instance_id = sys.argv[5]
+                gcp_instance_delete(project, zone, instance_id)
         else:
-            sys.exit('Usage: gcpclient instance {list|get|create}')
+            sys.exit('Usage: gcpclient instance {list|get|create|delete}')
+
+
+def project_list():
+    try:
+        request = gcp_credential('cloudresourcemanager').projects()
+        response = request.list().execute()
+        encodedjson =  json.dumps(response['projects'], sort_keys=True, indent=4)
+        print encodedjson
+    except:
+        sys.exit("get project list fail.")
+
+
+def project_create(name, project_id):
+    try:
+        config = {
+            'project_id': project_id,
+            'name': name
+        }
+        request = gcp_credential('cloudresourcemanager').projects()
+        response = request.create(body=config).execute()
+        print("create [%s] project success" % name)
+    except:
+        sys.exit("create project fail.")
+
+
+def gcp_project():
+    if len(sys.argv) < 3:
+        sys.exit('Usage: gcpclient project {list|create}')
+    else:
+        if sys.argv[2] == 'list':
+            if len(sys.argv) != 3:
+                message_alert(
+                    "Usage: gcpclient project list",
+                    "list all project infomation"
+                )
+            else:
+                project_list()
+        elif sys.argv[2] == 'create':
+            if len(sys.argv) != 5:
+                message_alert(
+                    "Usage: gcpclient project create {project name} {project ID}",
+                    "list all project infomation"
+                )
+            else:
+                name = sys.argv[3]
+                project_id = sys.argv[4]
+                project_create(name, project_id)
 
 
 def message_alert(usage, description):
@@ -143,10 +205,12 @@ def message_alert(usage, description):
 
 def main():
     if len(sys.argv) < 2:
-        sys.exit('Usage: gcpclient {instance}')
+        sys.exit('Usage: gcpclient {instance|project}')
     else:
         if sys.argv[1] == 'instance':
             gcp_instance()
+        elif sys.argv[1] == 'project':
+            gcp_project()
 
 
 if __name__ == '__main__':
